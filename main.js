8350f3363017b9f34f1735280d9d1854482a1cce
Muhammad Mubeen Maroof <119948066+mubeenmaroof@users.noreply.github.com>
2026-01-23 10:12:20 +0500

    Advance Coordinate Converter
    Complete

---
+++ main.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    icon: path.join(__dirname, "icon.png"),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile("index.html");
+
+  // Open DevTools in development (optional)
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
